3bb16436a65021abc44452e0db12c7e438605595
DAB0mB <emanor6@gmail.com>
2023-07-29 22:22:46 -0400

    Rename to unlisteners

--- src/events/effect.ts
+++ src/events/effect.ts
@@ -2,7 +2,7 @@ import { Event } from './event';
 import { Emitter, Listener } from './emitter';
 
 export class Effect extends Event {
-  private readonly offCache = new Set<() => void>();
+  private readonly unlisteners = new Set<() => void>();
 
   constructor(emitter: Emitter, private readonly events: Event[]) {
     for (const event of events) {
@@ -16,25 +16,25 @@ export class Effect extends Event {
 
   override on(listener: Listener) {
     const offEffect = super.on(listener);
-    this.offCache.add(offEffect)
+    this.unlisteners.add(offEffect)
 
-    if (this.offCache.size == 1) {
+    if (this.unlisteners.size == 1) {
       const emit = () => this.emit();
 
       for (const event of this.events) {
         const offEvent = event.on(emit);
-        this.offCache.add(offEvent);
+        this.unlisteners.add(offEvent);
       }
     }
 
     return () => {
       offEffect();
-      this.offCache.delete(offEffect);
+      this.unlisteners.delete(offEffect);
 
-      if (this.offCache.size == this.events.length) {
-        for (const offEvent of this.offCache) {
+      if (this.unlisteners.size == this.events.length) {
+        for (const offEvent of this.unlisteners) {
           offEvent();
-          this.offCache.delete(offEvent);
+          this.unlisteners.delete(offEvent);
         }
       }
     };
