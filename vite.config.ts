cdefe0aa716b4949fa2156fdb70bc2daf9dd822a
DAB0mB <emanor6@gmail.com>
2023-07-30 15:27:19 -0400

    Add base URL

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/parallax-chess',
   plugins: [react()],
   resolve: {
     alias: [
